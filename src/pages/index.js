d204026ef74dd6993391b29668531871e8cf2f60
rahulkrish1990 <74992378+rahulkrish1990@users.noreply.github.com>
2020-11-25 03:35:59 +0530

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,81 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Strong, Text, Box, Image } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { Override, Menu, Section } from "@quarkly/components";
+export default (() => {
+	return <Theme theme={theme}>
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Section>
+			<Box
+				display="flex"
+				padding="12px 0"
+				justify-content="space-between"
+				align-items="center"
+				flex-direction="row"
+				md-flex-direction="column"
+			>
+				<Text margin="0" md-margin="0px 0 20px 0" text-align="left" font="--lead">
+					<Strong>
+						Rahul krishnan
+					</Strong>
+				</Text>
+				<Menu
+					display="flex"
+					justify-content="center"
+					font="--base"
+					font-weight="700"
+					md-flex-direction="column"
+					md-align-items="center"
+				>
+					<Override slot="link" text-decoration="none" color="--dark" padding="6px 12px" />
+					<Override slot="link-active" color="--primary" />
+					<Override slot="item" padding="6px" />
+				</Menu>
+			</Box>
+		</Section>
+		<Section text-align="center" padding="100px 0" sm-padding="40px 0">
+			<Text as="h1" font="--headline1" md-font="--headline2" margin="20px 0 0 0">
+				About Me
+			</Text>
+			<Text as="p" font="--lead" margin="20px 0 0 0">
+				Hi! I'm a paragraph. Click here to add your own text and edit me. It’s a piece of cake. I’m a great space for you to tell a story and let your site visitors know more about you. Talk about your business and what products and services you offer. Share how you came up with the idea for your company and what makes you different from your competitors. Make your business stand out and show your visitors who you are.{" "}
+			</Text>
+			<Box display="flex" margin="40px 0 20px 0" justify-content="space-around" sm-flex-direction="column">
+				<Box padding="10px">
+					<Image src="https://uploads.quarkly.io/5fbd8092b83a2f001f2e31c9/images/q.jpg?v=2020-11-24T22:02:11.592Z" width="320px" max-width="100%" mix-blend-mode="normal" />
+				</Box>
+				<Box padding="10px" />
+				<Box padding="10px" />
+			</Box>
+		</Section>
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+	</Theme>;
+});
---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fbd8092b83a2f001f2e31cc",
+				"5fbd8092b83a2f001f2e31ce"
+			]
+		},
+		"5fbd8092b83a2f001f2e31cc": {
+			"id": "5fbd8092b83a2f001f2e31cc",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fbd8092b83a2f001f2e31ce": {
+			"id": "5fbd8092b83a2f001f2e31ce",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
